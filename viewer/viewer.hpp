2ab3edfa697e8e388a2d6ecb7bd7095f68838fc1
kvnptl <kevinpatel4400@gmail.com>
2024-07-03 01:44:22 +0200

    [viewer] add namespace

--- viewer/viewer.hpp
+++ viewer/viewer.hpp
@@ -2,4 +2,7 @@
 #include <Eigen/Core>
 #include <vector>
 
-void viewCloud(const std::vector<Eigen::Vector2d> &pcd);
+namespace viewer
+{
+    void viewCloud(const std::vector<Eigen::Vector2d> &pcd);
+} // namespace viewer
